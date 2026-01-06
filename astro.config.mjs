4a2e3209a904dc8cc08718ed3175f1d69c86183c
TadeSourE <2764427463@qq.com>
2026-01-06 20:31:49 +0800

    更新config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(),tailwind()]
+  // ✅ 只有 integrations 才是对的
+  integrations: [
+    react(),
+    mdx(),
+    tailwind() 
+  ]
 });
